4d37cb4cd79ed5354837fd3940724393e23b9bbc
Damian Lasoń <lasondamian9@gmail.com>
2020-06-12 21:58:38 +0200

    Added constructors

--- src/fields/TextAreaField.ts
+++ src/fields/TextAreaField.ts
@@ -3,7 +3,13 @@ import { Field } from "../interfaces/Field";
 export class TextAreaField implements Field {
   name: string;
   label: string;
-  type: import("../enums/FieldType").FieldType;
+  type: import("../enums/FieldType").FieldType.Textarea;
   value: string;
   render: () => HTMLDivElement;
+
+  constructor(name: string, label: string, value: string) {
+    this.name = name;
+    this.label = label;
+    this.value = value;
+  }
 }
