70f8b127b6ec33e4245e58d56d8e2669eb8bd7ce
damianlason <lasondamian9@gmail.com>
2020-06-28 06:23:48 +0200

    getDocuments method implemented + added helper methods

--- src/LocStorage.ts
+++ src/LocStorage.ts
@@ -1,10 +1,15 @@
 import { Storage } from "./interfaces/Storage.js";
 
 export class LocStorage implements Storage {
+  constructor() {
+    this.initDocList();
+  }
+
   saveDocument = (formData: FormData): string => {
     const docId: string = this.generateStorageId();
     let formDataToSave = this.prepareFormData(formData);
 
+    this.addToDocList(docId);
     localStorage.setItem(docId, formDataToSave);
 
     return docId;
@@ -14,7 +19,21 @@ export class LocStorage implements Storage {
     return localStorage.getItem(docId);
   };
 
-  getDocuments: () => string[];
+  getDocuments = (): string[] => {
+    return JSON.parse(localStorage.getItem("docList")!);
+  };
+
+  private addToDocList = (docId: string): void => {
+    let docList: string[] = JSON.parse(localStorage.getItem("docList")!);
+    docList.push(docId);
+    localStorage.setItem("docList", JSON.stringify(docList));
+  };
+
+  private initDocList = (): void => {
+    if (localStorage.getItem("docList") === null) {
+      localStorage.setItem("docList", JSON.stringify([]));
+    }
+  };
 
   private generateStorageId = (): string => {
     return "document-" + Date.now();
