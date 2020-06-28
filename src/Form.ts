6da0af7c9c68772dc36f40f343927bbb9895aa08
damianlason <lasondamian9@gmail.com>
2020-06-28 02:21:27 +0200

    Code formatted

--- src/Form.ts
+++ src/Form.ts
@@ -4,51 +4,62 @@ import { EmailField } from "./fields/EmailField.js";
 import { SelectField } from "./fields/SelectField.js";
 import { CheckboxField } from "./fields/CheckboxField.js";
 import { TextAreaField } from "./fields/TextAreaField.js";
+import { FieldType } from "./enums/FieldType.js";
 
 export class Form {
-  formContainer = document.querySelector('#form-container') as HTMLDivElement;
+  formContainer = document.querySelector("#form-container") as HTMLDivElement;
   allFields: Field[];
 
-  constructor()
-  {
-    this.allFields= [
-      new InputField('nameField', 'Imię'),
-      new InputField('surnameField', 'Nazwisko'),
-      new EmailField('emailField', 'Adres Email'),
-      new SelectField('selectField', 'Wybrany kierunek studiów', ['Kognitywistyka', 'Logistyka', 'Informatyka', 'Filozofia']),
-      new CheckboxField('checkboxField', 'Czy preferujesz e-learning?'),
-      new TextAreaField('textareaField', 'Uwagi')
+  constructor() {
+    this.allFields = [
+      new InputField("nameField", "Imię"),
+      new InputField("surnameField", "Nazwisko"),
+      new EmailField("emailField", "Adres Email"),
+      new SelectField("selectField", "Wybrany kierunek studiów", [
+        "Kognitywistyka",
+        "Logistyka",
+        "Informatyka",
+        "Filozofia",
+      ]),
+      new CheckboxField("checkboxField", "Czy preferujesz e-learning?"),
+      new TextAreaField("textareaField", "Uwagi"),
     ];
 
     this.render();
   }
 
   getValue = () => {
-    this.allFields.forEach(field => {
-      let fieldDOM = document.querySelector('[name="' + field.name + '"]') as HTMLInputElement;
-      console.log(fieldDOM.value);
+    this.allFields.forEach((field) => {
+      let fieldDOM = document.querySelector(
+        '[name="' + field.name + '"]'
+      ) as HTMLInputElement;
+      if (field.type === FieldType.Checkbox) {
+        console.log(fieldDOM.checked);
+      } else {
+        console.log(fieldDOM.value);
+      }
     });
   };
 
   render = () => {
-      let form = document.createElement('form');
+    let form = document.createElement("form");
 
-      this.allFields.forEach(field => {
-        form.appendChild(field.render());
-      });
+    this.allFields.forEach((field) => {
+      form.appendChild(field.render());
+    });
 
-      let submitButton = document.createElement('button');
-      submitButton.type = 'submit';
-      submitButton.className = 'btn btn-primary';
-      submitButton.innerText = 'Wyślij';
+    let submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.className = "btn btn-primary";
+    submitButton.innerText = "Wyślij";
 
-      form.appendChild(submitButton);
+    form.appendChild(submitButton);
 
-      form.addEventListener('submit', (e: Event) => {
-        e.preventDefault();
-        this.getValue();
-      });
+    form.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+      this.getValue();
+    });
 
-      this.formContainer.appendChild(form);
+    this.formContainer.appendChild(form);
   };
 }
