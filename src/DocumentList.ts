a61a0be8b3e80aa0c7df987db8e5bad382c7a2f3
damianlason <lasondamian9@gmail.com>
2020-06-30 08:49:00 +0200

    DocumentList render method implemented

--- src/DocumentList.ts
+++ src/DocumentList.ts
@@ -1,6 +1,9 @@
 import { LocStorage } from "./LocStorage";
 
 export class DocumentList {
+  docListContainer = document.querySelector(
+    "#doc-list-container"
+  ) as HTMLDivElement;
   docList: string[];
 
   getDocumentList = (): void => {
@@ -8,5 +11,32 @@ export class DocumentList {
     this.docList = locStorage.getDocuments();
   };
 
-  render: () => {};
+  render = () => {
+    let table = document.createElement("table");
+    table.className = "table";
+
+    let tableHeader = document.createElement("thead");
+
+    let tableHeaderRecord = document.createElement("tr");
+    let tableHeaderRecordContent = document.createElement("th");
+    tableHeaderRecordContent.innerText = "ID dokumentu";
+
+    tableHeaderRecord.appendChild(tableHeaderRecordContent);
+    tableHeader.appendChild(tableHeaderRecord);
+    table.appendChild(tableHeader);
+
+    let tableBody = document.createElement("tbody");
+
+    this.docList.forEach((docID) => {
+      let tableBodyRecord = document.createElement("tr");
+      let tableRecordData = document.createElement("td");
+      tableRecordData.innerText = docID;
+      tableBodyRecord.appendChild(tableRecordData);
+      tableBody.appendChild(tableBodyRecord);
+    });
+
+    table.appendChild(tableBody);
+
+    this.docListContainer.appendChild(table);
+  };
 }
