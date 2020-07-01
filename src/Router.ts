9c7694a07d5ef9bc51f1ad96ed45fc740f9e7a55
damianlason <lasondamian9@gmail.com>
2020-07-01 06:29:52 +0200

    router added

---
+++ src/Router.ts
@@ -0,0 +1,12 @@
+export class Router {
+  constructor() {}
+
+  static getParam(key: string) {
+    const query: string = window.location.search.substr(
+      0,
+      window.location.search.length
+    );
+    const urlParams = new URLSearchParams(query);
+    return urlParams.get("id");
+  }
+}
